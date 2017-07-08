1635b85fa689fa5534bc677bd577ce8c8eff2dd6
bilijo <damir@netcourrier.com>
2017-07-08 06:56:13 +0200

    add network state check

--- app/src/main/java/com/example/android/quakereport/EarthquakeDataActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeDataActivity.java
@@ -5,6 +5,8 @@ import android.content.res.Resources;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -42,12 +44,23 @@ public class EarthquakeDataActivity extends AppCompatActivity  implements Loader
 
 
     public static final String LOG_TAG = EarthquakeDataActivity.class.getName();
+
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
     /**
      * Sample JSON response for a USGS query
      */
+    // Test message display then the url didn't match
+  //  private static final String USGS_REQUEST_URL ="";
+
+
+
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+
     public static Uri earthquakeUri;
+
 
 
     /** Adapter for the list of earthquakes */
@@ -62,6 +75,8 @@ public class EarthquakeDataActivity extends AppCompatActivity  implements Loader
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(list);
 
+
+
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new EarthQuakeDataAdapter(this, new ArrayList<EarthQuakeData>());
 
@@ -69,6 +84,13 @@ public class EarthquakeDataActivity extends AppCompatActivity  implements Loader
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(mAdapter);
 
+        /*Now we need to hook up the TextView as the empty view of the ListView.
+        We can use the ListView setEmptyView() method. We can also make the empty state TextView be a global variable,
+         so we can refer to it in a later method.
+        The TextView class was also automatically imported into the java file, as soon as we used that class.
+         */
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(mEmptyStateTextView);
 
         // Set an item click listener on the ListView, which sends an intent to a web browser
         // to open a website with more information about the selected earthquake.
@@ -90,13 +112,31 @@ public class EarthquakeDataActivity extends AppCompatActivity  implements Loader
         });
 
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            //View loadingIndicator = findViewById(R.id.loading_indicator);
+           // loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
 
     }
 
@@ -119,6 +159,8 @@ public class EarthquakeDataActivity extends AppCompatActivity  implements Loader
         if (earthQuakeDatas != null && !earthQuakeDatas.isEmpty()) {
             mAdapter.addAll(earthQuakeDatas);
         }
+
+
     }
 
     @Override
